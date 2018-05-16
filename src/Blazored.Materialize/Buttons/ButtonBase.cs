dba7b83f1a8a1256b2eea41d7a9a6c0fb8674da9
Chris Sainty <chrissainty@users.noreply.github.com>
2018-05-16 12:54:43 +0100

    Changed isdisabled to disabled as it seems a nicer api

--- src/Blazored.Materialize/Buttons/ButtonBase.cs
+++ src/Blazored.Materialize/Buttons/ButtonBase.cs
@@ -4,7 +4,7 @@ namespace Blazored.Materialize.Buttons {
 
     public class ButtonBase : BlazorComponent {
         
-        [Parameter] protected bool IsDisabled { get; set ;}
+        [Parameter] protected bool Disabled { get; set ;}
         [Parameter] protected bool IsSmall { get; set; }
         [Parameter] protected bool IsLarge { get; set; }
     }
